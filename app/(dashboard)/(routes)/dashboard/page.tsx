1fecbf1170b4ad0d7daad5eeb9207bb50cc43448
ShahinurAlamBhuiyan <shahinur.alam.bhuiyan01@gmail.com>
2023-08-11 10:20:28 +0600

    setup environment & routing

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+export default function DashboardPage() {
+    return (
+        <div className='text-6xl text-green-500'>
+            DashboardPage (Protected)
+        </div>
+    )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+
+const LandingPage = () => {
+    return (
+        <div>LandingPage (Unprotected)</div>
+    )
+}
+
+export default LandingPage
